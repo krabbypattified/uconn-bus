245356ed3503b6d2a859ad429452433eca4f1d61
Gabe Rogan <gabriel.rogan@uconn.edu>
2017-10-19 00:51:44 -0400

    freemarker 70% done

--- src/components/FreeMarker.js
+++ src/components/FreeMarker.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import MapboxGL from 'mapbox-gl'
 import Hammer from 'hammerjs'
 
 
@@ -10,13 +11,44 @@ export default class FreeMarker extends React.Component {
     map: PropTypes.any
   }
 
+  project(lngLat) {
+    if (this.projected === false) this.markerDiv.remove() // remove from DOM tree
+
+    if (!lngLat) {
+      // translate(x,y) => lngLat
+      const matrix = window.getComputedStyle(this.markerDiv).getPropertyValue('transform')
+      const translate = matrix.match(/\d+/g) || [0,0,0,0,0,0]
+      const xy = [translate[4],translate[5]]
+      lngLat = this.context.map.project(xy) // TODO BUG i think we're off by a few pixels
+    }
+
+    if (!this.marker) this.marker = new MapboxGL.Marker(this.markerDiv)
+    this.marker.setLngLat(lngLat).addTo(this.context.map)
+    // TODO make sure you can drag it once it becomes a marker
+  }
+
+  unproject(position) {
+    if (this.projected) this.marker.remove() // remove from map
+
+    if (!position) {
+      // The marker keeps its transform property.
+      // If no transform property, no need to initialize one
+    }
+    else {
+      // TODO top, right, bottom, left => translate(calc(-50% + x), calc(-50% + y))
+      this.markerDiv.style.transform = '...'
+    }
+
+    this.context.map.getContainer().appendChild(this.markerDiv)
+  }
+
   componentWillMount() {
     let map = this.context.map
-    let mapdiv = map.getContainer()
+
     this.markerDiv = document.createElement('div')
     this.markerDiv.style.position = 'absolute'
-    mapdiv.appendChild(this.markerDiv)
 
+    // TODO easy fix for DRAGGABLE MARKER!! (initialize to translate matrix)
     let x = 0
     let y = 0
 
@@ -36,7 +68,15 @@ export default class FreeMarker extends React.Component {
     })
   }
 
+  componentWillUpdate(newProps) {
+    this.same =
+      this.props.projected === newProps.projected &&
+      this.props.position === newProps.position &&
+      this.props.lngLat === newProps.lngLat
+  }
+
   render() {
+    if (!this.same) this.props.projected ? this.project(this.props.lngLat) : this.unproject(this.props.position)
     return ReactDOM.createPortal(
       this.props.children,
       this.markerDiv,
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Map from 'components/Map'
-import Marker from 'components/Marker'
+// import Marker from 'components/Marker'
 import FreeMarker from 'components/FreeMarker'
 
 
@@ -13,9 +13,10 @@ export default class App extends React.Component {
         center={[-72.2683646, 41.8059531]}
         zoom={12}
         >
-        <Marker lngLat={[-72.2683646, 41.8059531]}>hey there bud</Marker>
-        <FreeMarker>hey there kids</FreeMarker>
-        <div style={{position:'absolute',bottom:0,zIndex:100}}>hi i'm down here!</div>
+        {/* <Marker lngLat={[-72.2683646, 41.8059531]}>hey there bud</Marker> */}
+        <FreeMarker projected lngLat={[-72.2683646, 41.8059531]}>hey there kids</FreeMarker>
+        {/* <FreeMarker lock lngLat={[-72.2683646, 41.8059531]} position={{top:0,left:0}}>hey there kids</FreeMarker> */}
+        {/* <div style={{position:'absolute',bottom:0,zIndex:100}}>hi i'm down here!</div> */}
       </Map>
     )
   }
