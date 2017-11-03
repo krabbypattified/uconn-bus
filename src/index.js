ec492bfbbe36512d85570ff04d95c18ee06768cb
Gabe Rogan <gabriel.rogan@uconn.edu>
2017-11-03 12:27:21 -0400

    pwa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'containers/App'
+import registerServiceWorker from 'assets/registerServiceWorker'
 import 'assets/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
